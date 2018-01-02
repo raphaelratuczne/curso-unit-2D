e2e6def7d363a57a499978768e3f260b37d6257e
Unknown <raphaelratuczne@gmail.com>
2018-01-02 17:03:33 -0200

    add files

---
+++ Assets/Scripts/MovePato.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePato : MonoBehaviour {
+
+	public float vel = 2.5f;
+	public int moedas = 0;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			transform.Translate (new Vector2 (vel * Time.deltaTime, 0));
+		}
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			transform.Translate (new Vector2 (-vel * Time.deltaTime, 0));
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D outro) {
+		if ( outro.gameObject.CompareTag("moeda") ) {
+			moedas++;
+			Destroy ( outro.gameObject );
+		}
+	}
+}
---
+++ Assets/Scripts/Variaveis.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variaveis : MonoBehaviour {
+
+	private float vel = 2.5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		/*if ( Input.GetKey(KeyCode.RightArrow) ) {
+			transform.Translate ( new Vector3(vel * Time.deltaTime, 0, 0) );
+		}
+		if ( Input.GetKey(KeyCode.LeftArrow) ) {
+			transform.Translate ( new Vector3(-vel * Time.deltaTime, 0, 0) );
+		}*/
+
+		float H = Input.GetAxis ("Horizontal");
+		float V = Input.GetAxis ("Vertical");
+		transform.Translate (new Vector3 (H * Time.deltaTime, V * Time.deltaTime, 0));
+
+	}
+}
